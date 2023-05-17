31a8f8441cf9a3e23606bd8c1f17774ee28824f5
SingleCellCrohns <30318308+WernaUVenema@users.noreply.github.com>
2023-05-17 08:19:02 +0200

    Update filtering_and_QC.R with referencedata and cellnumbers

--- DDTx/filtering_and_QC.R
+++ DDTx/filtering_and_QC.R
@@ -5,10 +5,12 @@ data<-readRDS("/source/ddtx_merged_demultiplexed_clustered_compartment_azi_elmen
 
 # filter 60% mito
 data<-subset(data, subset=percent.mito < 60)
+# reduction of 96579 to 93191 cells
 
 # exclude T8. patient 3 (wrongly sampled, only recipient cells)
 data@meta.data$pt_project_lane<-paste(data@meta.data$lane,data@meta.data$donor_final, sep="_")
 data<-subset(data, subset=pt_project_lane!="220504_lane04_UMCGDDtx00005r")
+# reduction of 93191 to 90580 cells
 
 #save
 saveRDS(data, "/source/ddtx_merged_demultiplexed_clustered_compartment_azi_elmentaiteadultileum_below60pctmito.rds")
@@ -71,6 +73,25 @@ data@meta.data$patient_compartment <- paste(data@meta.data$donor_final,data@meta
 unique(data@meta.data$patient_compartment)
 data_2 <- subset(data, patient_compartment != "UMCGDDtx00003r_Epithelial" & patient_compartment != "UMCGDDtx00004r_Epithelial" & patient_compartment !="UMCGDDtx00005r_Epithelial")
 unique(data_2@meta.data$patient_compartment)
+dim(data_2)
+#reduction 90580 to 90282 cells
+
+## add clinical reference
+
+reference<-read.csv("/source/reference_2.csv", sep=";")
+reference<-reference[-c(1,10)]
+reference<-reference[-18,]
+
+meta<-data_2@meta.data
+meta$cellname<-rownames(meta)
+dim(meta)
+meta<-merge(meta, reference, by="pt_project_lane", all=T)
+dim(meta)
+meta<-meta[c(52:61)]
+head(meta)
+rownames(meta)<-meta$cellname
+meta<-meta[-1]
+data_2<-AddMetaData(data_2, meta)
 
 # save
 saveRDS(data_2, "/source/ddtx_merged_demultiplexed_clustered_compartment_azi_elmentaiteadultileum_below60pctmito_withoutEpithelialR.rds")
